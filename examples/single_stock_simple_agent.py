dfd36d7755dddc6958d76e920ce2043a728086ff
alexander paskal <alexcpaskal@gmail.com>
2024-08-25 17:04:37 -0400

    refactroed to conform to new interface

--- examples/single_stock_simple_agent.py
+++ examples/single_stock_simple_agent.py
@@ -10,12 +10,13 @@ from swing_trader_env.types import BuyAction, SellAction
 
 def main():
     # instantiate the env
-    env = SingleStockEnv()
+    env = SingleStockEnv(
+        ticker="AAPL",
+        date="2020-03-20",
+        principal=10000,
+        frequency="daily"
+    )
 
-    # specify a Ticker/Date
-    env.set_ticker("AAPL")
-    env.set_date("2020-03-20")
-    env.set_frequency("daily")
     # step the environment 
     for i in range(100):
 
@@ -24,16 +25,20 @@ def main():
             env.step()
             continue
         
-        # buy on every on-20th action
+        # buy on every on-20th action - Invest 90% of liquid capital
         elif i % 20 == 0:
-            buy_action = BuyAction(ticker="AAPL",shares=1)
+            buy_action = BuyAction(ticker="AAPL",shares= (0.9 * env.cash) / env.close_price)
             env.step(buy_action)
 
-        # sell on every off-20th action
+        # sell on every off-20th action - Liquidate all shares held
         else:
-            sell_action = SellAction(ticker="AAPL", shares=1)
+            sell_action = SellAction(ticker="AAPL", shares=env.shares_held)
             env.step(sell_action)
-        
+    
+    # print portfolio performance
+    print(f"principal:       {env.principal}")
+    print(f"portfolio value: {env.net_worth}")
+    print(f"performance:     {env.performance}")
 
     # render the results
     env.render()
